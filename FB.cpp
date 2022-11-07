b4e948d358e66ec5dbe096317e588662041458e5
Matthew Lowe <lowe.matthew@mail.com>
2022-11-07 12:48:42 +0000

    🌽

--- FB.cpp
+++ FB.cpp
@@ -1,4 +1,4 @@
-#include <fstream>
+﻿#include <fstream>
 #include <string>
 #include <iostream>
 #include <map>
@@ -12,6 +12,7 @@ struct Message
     double Reactions;
     double NumLaughing;
     int Messages;
+    int Corn;
 };
 
 int main(int argc, char* argv[])
@@ -47,8 +48,9 @@ int main(int argc, char* argv[])
     std::cout << std::endl;
 
     std::vector<Message> Results;
-    std::map<std::string, int> Funniest;
-    std::map<std::string, int> MostMessages;
+    std::map<std::string, double> Funniest;
+    std::map<std::string, double> MostMessages;
+    std::map<std::string, double> Corn;
 
     for (const std::string& raw : rawfiles)
     {
@@ -71,7 +73,7 @@ int main(int argc, char* argv[])
 
                     for (picojson::value react : reactions)
                     {
-                        if (react.get<picojson::object>()["reaction"].get<std::string>() == "\\u00f0\\u009f\\u0098\\u0086")
+                        if (react.get<picojson::object>()["reaction"].get<std::string>() == "laughing")
                         {
                             num_laughing++;
                         }
@@ -80,13 +82,17 @@ int main(int argc, char* argv[])
                     Results.push_back({
                         msgobj["sender_name"].get<std::string>(),
                         msgobj["content"].is<std::string>() ? msgobj["content"].get<std::string>() : "N/A",
-                        static_cast<double>(reactions.size()), num_laughing });
+                        num_laughing, num_laughing });
 
-                    Funniest[msgobj["sender_name"].get<std::string>()] += reactions.size();
-                    
+                    Funniest[msgobj["sender_name"].get<std::string>()] += num_laughing;
                 }
 
                 MostMessages[msgobj["sender_name"].get<std::string>()]++;
+
+                if (msgobj["content"].is<std::string>() && msgobj["content"].get<std::string>() == "corn")
+                {
+                    Corn[msgobj["sender_name"].get<std::string>()]++;
+                }
             }
         }
         else
@@ -99,7 +105,7 @@ int main(int argc, char* argv[])
         return x.Reactions > y.Reactions;
     });
 
-    const int max = 10;
+    const int max = 20;
 
     for (int i = 0; i < Results.size() && i < max; ++i)
     {
@@ -112,7 +118,7 @@ int main(int argc, char* argv[])
 
     for (const auto& R : Funniest)
     {
-        FunniestSorted.push_back({ R.first, "", static_cast<double>(R.second) / MostMessages[R.first] });
+        FunniestSorted.push_back({ R.first, "", R.second / MostMessages[R.first] });
     }
 
     std::sort(FunniestSorted.begin(), FunniestSorted.end(), [](const Message& x, const Message& y) {
@@ -120,6 +126,24 @@ int main(int argc, char* argv[])
     });
 
     for (const auto& R : FunniestSorted)
+    {
+        std::cout << R.Person << " - " << R.Reactions << std::endl;
+    }
+
+    std::cout << std::endl << "Corn" << std::endl;
+
+    std::vector<Message> CornSorted;
+
+    for (const auto& R : Corn)
+    {
+        CornSorted.push_back({ R.first, "", R.second });
+    }
+
+    std::sort(CornSorted.begin(), CornSorted.end(), [](const Message& x, const Message& y) {
+        return x.Reactions > y.Reactions;
+    });
+
+    for (const auto& R : CornSorted)
     {
         std::cout << R.Person << " - " << R.Reactions << std::endl;
     }
